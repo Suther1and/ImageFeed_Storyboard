99c640e7f93d160f3b0b42af595b7c6c1575323f
suther1and <aiur272@gmail.com>
2024-07-15 16:26:24 +0300

    sprint_10
    url code func done

--- ImageFeed_Storyboard/Auth/WebViewViewController.swift
+++ ImageFeed_Storyboard/Auth/WebViewViewController.swift
@@ -23,10 +23,25 @@ class WebViewViewController: UIViewController {
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
         loadAuthView()
       }
     
-
+    //MARK: - Private Methods
+    private func code(from navigationAction: WKNavigationAction) -> String? {
+        if
+            let url = navigationAction.request.url,
+            let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        { 
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
+    
    //MARK: - URLComponents Setup
     private func loadAuthView() {
         guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
@@ -49,4 +64,19 @@ class WebViewViewController: UIViewController {
         webView.load(request)
     }
 
+}
+
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        if let code = code(from: navigationAction) {
+            //TODO: process code
+            decisionHandler(.cancel)
+        }else{
+            decisionHandler(.allow)
+        }
+    }
 }
