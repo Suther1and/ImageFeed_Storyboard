9f0ed89e09ece956e28673632c7a651271ae5d10
suther1and <aiur272@gmail.com>
2024-07-17 15:37:37 +0300

    Update SplashViewController.swift

--- ImageFeed_Storyboard/Splash/SplashViewController.swift
+++ ImageFeed_Storyboard/Splash/SplashViewController.swift
@@ -78,7 +78,7 @@ extension SplashViewController: AuthViewControllerDelegate {
         oAuth2Service.fetchOAuthToken(code: code) { [weak self] result in
             guard let self = self else { return }
             switch result {
-            case .success(let token):
+            case .success(_):
                 self.switchToTabBarController()
             case .failure(let error):
                 print("Error: \(error.localizedDescription)")
